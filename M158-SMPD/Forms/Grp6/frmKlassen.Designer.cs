80d06813cc762522e6f168eb8a101aa77ea38736
hedingerm <50134293+hedingerm@users.noreply.github.com>
2019-04-30 10:44:04 +0200

    Init

--- M158-SMPD/Forms/Grp6/frmKlassen.Designer.cs
+++ M158-SMPD/Forms/Grp6/frmKlassen.Designer.cs
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // frmKlassen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(370, 261);
+            this.Name = "frmKlassen";
             this.Text = "frmKlassen";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
