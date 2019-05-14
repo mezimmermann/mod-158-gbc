c787e55d4411df5118a242c13c58a9639e935416
Niels <nisc@n3xus.io>
2019-05-14 12:05:52 +0200

    Merge Grp2

--- M158-SMPD/Forms/Grp2/frmFaecherzuteilung.cs
+++ M158-SMPD/Forms/Grp2/frmFaecherzuteilung.cs
@@ -6,15 +6,80 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
 
 namespace M158_SMPD.Forms
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 {
     public partial class frmFaecherzuteilung : Form
     {
+        MySQLCon mysql = new MySQLCon();                //Variablen werden hier definiert
+        string[] ArrDatatable;
+        string[] ArrClass;
+        DataTable DtClass;
+        DataTable DtClassTotal;
         public frmFaecherzuteilung()
         {
-            InitializeComponent();
+            InitializeComponent();              //Initialisierung
+        }
+
+
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+        private void frmFaecherzuteilung_Load(object sender, EventArgs e)               //Laden der Form
+        {
+            DataTable DtDatatable = mysql.getSQLStatement("SELECT DISTINCT Kl_Nr FROM tbl_fae_kla_sem");            //Aus der Tabelle "tbl_fae_kla_sem" werden alle Klassen ausgewählt
+            ArrDatatable = DtDatatable.Rows.OfType<DataRow>().Select(k => k[0].ToString()).ToArray();
+            for (int i = 0; i < ArrDatatable.Length; i++)                                                           //i=0 wenn ArrDatatable grösser als 0 wird die ausgewählte Klasse angezeigt
+            {
+                if (i > 0)
+                {
+                    DtClass = mysql.getSQLStatement("SELECT klasse FROM tbl_klasse WHERE Kl_Nr = " + ArrDatatable[i]);      //Hier werden alle Klassen entsprechend der Kl_Nr angezeigt ansonsten passiert da gleiche
+                    DtClassTotal.Merge(DtClass);
+                }
+                else
+                {
+                    DtClassTotal= mysql.getSQLStatement("SELECT klasse FROM tbl_klasse WHERE Kl_Nr = " + ArrDatatable[i]);      //Hier werden nochmal alle Klassen entsprechend der Kl_Nr angezeigt
+                }
+            }
+            ArrClass = DtClassTotal.Rows.OfType<DataRow>().Select(k => k[0].ToString()).ToArray();
+            for (int i = 0; i < ArrClass.Length; i++)
+            {
+                CmxClass.Items.Add(ArrClass[i]);
+            }
+        }
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+        private void CmxClass_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            DataTable DtGrid = mysql.getSQLStatement("SELECT * FROM faecherzuteilung WHERE Klasse = '" + CmxClass.Text + "'");      //Statement welches die Informationen aus "faecherzuteilung" auswählt, welche man über die CmxClass ausgewählt hat
+            DgvView.DataSource = DtGrid;
+            DgvView.Columns["Se_Nr"].Visible = false;
+            DgvView.Columns["Klasse"].Visible = false;
+            DgvView.Columns["Fae_Nr"].Visible = false;
+            DgvView.Columns["Kl_Nr"].Visible = false;
+
+        }
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------      
+        private void DgvView_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            string StrQuery = "";
+            if(DgvView.Rows[e.RowIndex].Cells[e.ColumnIndex].Value.ToString() != "")    //Sobald der Wert im Gridview geändert wird, wir die COLUMN in der Tabelle geändert
+            {
+                if (DgvView.Columns[e.ColumnIndex].Name == "Semester")
+                {
+                    StrQuery = "UPDATE tbl_semester SET Semester='" + DgvView.Rows[e.RowIndex].Cells[e.ColumnIndex].Value.ToString() + "' WHERE Se_Nr=" + DgvView.Rows[e.RowIndex].Cells[3].Value.ToString();
+                }
+                else
+                {
+                    StrQuery = "UPDATE tbl_faecher SET Fach='" + DgvView.Rows[e.RowIndex].Cells[e.ColumnIndex].Value.ToString() + "' WHERE Fae_Nr=" + DgvView.Rows[e.RowIndex].Cells[4].Value.ToString();
+                }
+                mysql.setSQLStatement(StrQuery);
+            }
+            else
+            {
+                MessageBox.Show("Bitte geben Sie einen Wert ein");        //Bei keiner Eingabe (NOT NULL) erscheint eine Messagebox welche den Benutzer informiert
+                return;
+            }
         }
     }
 }
--- M158-SMPD/Forms/Grp2/frmNotenperiode.cs
+++ M158-SMPD/Forms/Grp2/frmNotenperiode.cs
@@ -12,9 +12,61 @@ namespace M158_SMPD.Forms
 {
     public partial class frmNotenperiode : Form
     {
+        MySQLCon mysql = new MySQLCon();        //Variablen werden hier definiert
+        string[] ArrDatatable;
+        string[] ArrClass;
+        DataTable DtClass;
+        DataTable DtClassTotal;
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------       
         public frmNotenperiode()
         {
-            InitializeComponent();
+            InitializeComponent();      //Initialisierung
+        }
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------  
+        private void frmNotenperiode_Load(object sender, EventArgs e)
+        {
+            NotenperiodeLoadList();     //Beim Öffnen der Form wird die Liste geladen
+        }
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------  
+        private void NotenperiodeLoadList()
+        {
+            CbxDrop.Items.Clear();
+            DataTable DtDatatable = mysql.getSQLStatement("SELECT DISTINCT * FROM tbl_semester");       //Alle Notenperioden werden ausgewählt
+            ArrDatatable = DtDatatable.Rows.OfType<DataRow>().Select(k => k[0].ToString()).ToArray();
+            for (int i = 0; i < ArrDatatable.Length; i++)
+            {
+                if (i > 0)
+                {
+                    DtClass = mysql.getSQLStatement("SELECT Semester FROM tbl_semester WHERE Se_Nr = " + ArrDatatable[i]);      //Hier werden alle Semester entsprechend der SE_Nr angezeigt
+                    DtClassTotal.Merge(DtClass);
+                }
+                else
+                {
+                    DtClassTotal = mysql.getSQLStatement("SELECT Semester FROM tbl_semester WHERE Se_Nr = " + ArrDatatable[i]);     //Hier werden alle Semester entsprechend der SE_Nr angezeigt
+                }
+            }
+            ArrClass = DtClassTotal.Rows.OfType<DataRow>().Select(k => k[0].ToString()).ToArray();
+            for (int i = 0; i < ArrClass.Length; i++)
+            {
+                CbxDrop.Items.Add(ArrClass[i]);
+            }            
+        }
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------  
+        private void CbxDrop_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            TbxEdit.Text = CbxDrop.Text;        //Der Text in der Combobox wir mit dem angepassten Text angepasst
+        }
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------  
+        private void button1_Click(object sender, EventArgs e)      //SAVE-Button: Was geschieht wenn der "Speicher-Button" geklickt wird.
+        {
+            string StrQuery1 = "Select Se_Nr FROM tbl_semester where Semester = '" + CbxDrop.Text + "'";        
+            DataTable Dtresult = mysql.getSQLStatement(StrQuery1);
+            int Intresult = Convert.ToInt16(Dtresult.Rows[0][0]);
+            string StrQuery2 = "UPDATE tbl_semester SET Semester='" + TbxEdit.Text + "' WHERE Se_Nr=" + Intresult;
+            mysql.setSQLStatement(StrQuery2);
+            NotenperiodeLoadList();
+            CbxDrop.Text = TbxEdit.Text;
+            MessageBox.Show("Semestername wurde zu " + TbxEdit.Text + " geändert!");       //Als Information für den Benutzer "Die Werte wurden geändert"
         }
     }
 }
