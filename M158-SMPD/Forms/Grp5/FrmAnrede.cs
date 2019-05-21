c12e65b04a6b6033d2626cac05d5498d1c07c2e7
Nopsibob <nopsibob@gmail.com>
2019-05-21 09:33:34 +0200

    Update GRP5 Try Catsch

--- M158-SMPD/Forms/Grp5/FrmAnrede.cs
+++ M158-SMPD/Forms/Grp5/FrmAnrede.cs
@@ -12,35 +12,56 @@ namespace M158_SMPD.Forms.Grp5
         public frmanrede()
         {
             InitializeComponent(); //Komponenten werden Inizialisiert
-            string sql = " SELECT * FROM tbl_anrede";//SQL Query wird erstellt
-            DataTable table = new DataTable(); // Datatable wird erstellt
+            try
+            {
+                string sql = " SELECT * FROM tbl_anrede";//SQL Query wird erstellt
+                DataTable table = new DataTable(); // Datatable wird erstellt
 
-            table = MYSQLconn.GetSqlStatement(sql); //Abfrage wird mit MySQLCon.cs durchgeführt
-            BSbinder.DataSource = table;//Daten werden Formatiert
-            DgvFrmanrededisplay.DataSource = BSbinder; //Daten werden in Data Grid View gelesen
+                table = MYSQLconn.GetSqlStatement(sql); //Abfrage wird mit MySQLCon.cs durchgeführt
+                BSbinder.DataSource = table;//Daten werden Formatiert
+                DgvFrmanrededisplay.DataSource = BSbinder; //Daten werden in Data Grid View gelesen
 
-            DgvFrmanrededisplay.Columns["An_Nr"].ReadOnly = true; // Primary ID wird zum Bearbeiten Deaktiviert
-        }
+                DgvFrmanrededisplay.Columns["An_Nr"].ReadOnly = true; // Primary ID wird zum Bearbeiten Deaktiviert
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Fehler beim Lesen. Starten Sie das Programm Neu.\n\n Error Message: \n\n " + e.Message);//Eine Messagebox wird ausgegeben das es ein  Fehler gibt.
+            }
+         }
 
         private void Btnsearchanrede_Click(object sender, EventArgs e)
         {
-            var tbxcontent = "%" + Tbxsearchanrede.Text + "%";//Suche wird integriert
-            string sql = " SELECT * FROM tbl_anrede WHERE tbl_anrede.Anrede LIKE '" + tbxcontent + "' OR tbl_anrede.An_Nr LIKE '" + tbxcontent + "'";//SQL Query wird erstellt
-            DataTable table = new DataTable();// Datatable wird erstellt
+            try
+            {
+                var tbxcontent = "%" + Tbxsearchanrede.Text + "%";//Suche wird integriert
+                string sql = " SELECT * FROM tbl_anrede WHERE tbl_anrede.Anrede LIKE '" + tbxcontent + "' OR tbl_anrede.An_Nr LIKE '" + tbxcontent + "'";//SQL Query wird erstellt
+                DataTable table = new DataTable();// Datatable wird erstellt
 
-            table = MYSQLconn.GetSqlStatement(sql); //Abfrage wird mit MySQLCon.cs durchgeführt
-            BSbinder.DataSource = table;//Daten werden Formatiert
-            DgvFrmanrededisplay.DataSource = BSbinder; //Daten werden in Data Grid View gelesen
+                table = MYSQLconn.GetSqlStatement(sql); //Abfrage wird mit MySQLCon.cs durchgeführt
+                BSbinder.DataSource = table;//Daten werden Formatiert
+                DgvFrmanrededisplay.DataSource = BSbinder; //Daten werden in Data Grid View gelesen
 
-            DgvFrmanrededisplay.Columns["An_Nr"].ReadOnly = true; // Primary ID wird zum Bearbeiten Deaktiviert
+                DgvFrmanrededisplay.Columns["An_Nr"].ReadOnly = true; // Primary ID wird zum Bearbeiten Deaktiviert
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Fehler beim Lesen. Starten Sie das Programm Neu.\n\n Error Message: \n\n " + ex.Message);//Eine Messagebox wird ausgegeben das es ein  Fehler gibt.
+            }
         }
 
         private void Btnsavanrede_Click(object sender, EventArgs e)
         {
-            DataTable changes = ((DataTable)BSbinder.DataSource).GetChanges();//Änderungen werden ind den Datable changes durchgeführt
-            MYSQLconn.SendDataTable(changes, "tbl_anrede");//Änderungen werden in die Datenbank geschrieben
-            ((DataTable)BSbinder.DataSource).AcceptChanges();//Die Angezeigte Datatable akzeptiert die Daten nicht mehr als Änderungen und sieht sie als normal an
-            MessageBox.Show("Gespeichert");//Eine Messagebox wird ausgegeben das es geschpeichert wurde.
+            try
+            {
+                DataTable changes = ((DataTable)BSbinder.DataSource).GetChanges();//Änderungen werden ind den Datable changes durchgeführt
+                MYSQLconn.SendDataTable(changes, "tbl_anrede");//Änderungen werden in die Datenbank geschrieben
+                ((DataTable)BSbinder.DataSource).AcceptChanges();//Die Angezeigte Datatable akzeptiert die Daten nicht mehr als Änderungen und sieht sie als normal an
+                MessageBox.Show("Gespeichert");//Eine Messagebox wird ausgegeben das es geschpeichert wurde.
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Fehler beim Schreiben");//Eine Messagebox wird ausgegeben das es geschpeichert wurde.
+            }
         }
     }
 }
