6dadf3bf11f338910ed7bdd331dde3790305aa1c
Walddeny-boy <da.schatzmann@gmail.com>
2019-05-21 09:07:59 +0200

    try catch MySQLCon

--- M158-SMPD/MySQLCon.cs
+++ M158-SMPD/MySQLCon.cs
@@ -144,29 +144,37 @@ namespace M158_SMPD
 
         public void SendDataTable(DataTable changedTable, string table)
         {
-            //Open connection
-            if (this.OpenConnection() == true)
+            try
             {
-                //Create Command
-                if (changedTable == null)
+                //Open connection
+                if (this.OpenConnection() == true)
                 {
+                    //Create Command
+                    if (changedTable == null)
+                    {
+                        //close connection
+                        this.CloseConnection();
+                        return;
+                    }
+                    MySqlDataAdapter mySqlDataAdapter = new MySqlDataAdapter("SELECT * FROM " + table, connection);
+                    MySqlCommandBuilder mcb = new MySqlCommandBuilder(mySqlDataAdapter);
+                    mySqlDataAdapter.UpdateCommand = mcb.GetUpdateCommand();
+                    mySqlDataAdapter.Update(changedTable);
+
                     //close connection
                     this.CloseConnection();
+
+                }
+                else
+                {
                     return;
                 }
-                MySqlDataAdapter mySqlDataAdapter = new MySqlDataAdapter("SELECT * FROM " + table, connection);
-                MySqlCommandBuilder mcb = new MySqlCommandBuilder(mySqlDataAdapter);
-                mySqlDataAdapter.UpdateCommand = mcb.GetUpdateCommand();
-                mySqlDataAdapter.Update(changedTable);
-
-                //close connection
-                this.CloseConnection();
-
             }
-            else
+            catch (Exception ex)
             {
-                return;
+                MessageBox.Show(ex.Message);
             }
+            
         }
         //<summary>
         //DataTable changes = ((DataTable)dataGridView1.DataSource).GetChanges();
